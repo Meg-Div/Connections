6eb1b89a64bb129b923a01c8245645a529921dc5
Megan <meganthielman@gmail.com>
2023-03-19 13:55:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // FILE ROUTES
-app.post("auth/register", upload.single("picture"));
+app.post("auth/register", upload.single("picture"), register);
 
 // MONGOOSE
 
