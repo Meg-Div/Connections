a6934b1125f8be583b468a21d8f5d6b1e79a440d
Megan <meganthielman@gmail.com>
2023-03-20 19:48:05 -0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,6 +7,9 @@ export const verifyToken = async (req, res, next) => {
     if (!token) {
       return res.status(403).send("Access denied.");
     }
+    if (token.startsWith("Bearer")) {
+      token = token.slice(7, token.length).trimLeft();
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
