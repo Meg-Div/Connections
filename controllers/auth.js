e49a4b37d2aa13520fd1712ae82319a688394610
Megan <meganthielman@gmail.com>
2023-03-20 19:35:43 -0700

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,8 +44,12 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email: email });
+
     if (!user) return res.status(400).json({ msg: "No such user." });
+
+    const isMatch = await bcrypt.compare(password, user.password);
   } catch (error) {
     res.status(500).json({ error: err.message });
   }
