4316a9f42b2e60fe1842b3954741235cb7b80d29
Megan <meganthielman@gmail.com>
2023-03-20 19:46:13 -0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,10 @@ import { Jwt } from "jsonwebtoken";
 export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
+
+    if (!token) {
+      return res.status(403).send("Access denied.");
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
