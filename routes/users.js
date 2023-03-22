4e64742d8a8aecc23a3609846f264e9ae047eba2
Megan <meganthielman@gmail.com>
2023-03-21 18:01:07 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -6,3 +6,10 @@ import {
 } from "..//controllers/users.js";
 
 import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+//READ
+
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
