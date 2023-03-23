2d9120cec8d8d826d6e202f64c0141b75e67c946
Megan <meganthielman@gmail.com>
2023-03-23 16:29:41 -0700

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,20 @@
 import mongoose from "mongoose";
 
-const postSchema = mongoose.Schema();
+const postSchema = mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+});
