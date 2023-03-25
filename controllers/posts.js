13ac691e060d06235591e43a49e9c8a70d563fa5
Megan <meganthielman@gmail.com>
2023-03-24 18:12:00 -0700

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -47,3 +47,21 @@ export const getUserPosts = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+//UPDATE
+
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    isLiked ? post.likes.delete(userId) : post.likes.set(userId, true);
+
+    const updatedPost = await Post.findByIdAndUpdate(id, { likes: post.likes });
+    res.status(200).json();
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
