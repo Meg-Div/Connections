449c6f75c607dd489ef4789e7f5cfa31b387e68f
Megan <meganthielman@gmail.com>
2023-04-05 19:59:49 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,17 @@ import Login from "scenes/login";
 import Profile from "scenes/profile";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
