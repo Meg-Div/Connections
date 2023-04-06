6431ea12d2908dc02f7e7d1e8aebd8fe4d857445
Megan <meganthielman@gmail.com>
2023-04-05 19:56:50 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "scenes/home";
+import Login from "scenes/login";
+import Profile from "scenes/profile";
 
 function App() {
   return <div className="app"></div>;
