318eb6dd8708b08d5a7e0bcd749f7de147410523
Megan <meganthielman@gmail.com>
2023-04-07 17:08:23 -0700

    Persist set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,21 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Home from "scenes/home";
 import Login from "scenes/login";
 import Profile from "scenes/profile";
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
