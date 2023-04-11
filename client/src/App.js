ea0d2cd8d7386462077b6c37a762e2e53dc46b20
Megan <meganthielman@gmail.com>
2023-04-10 18:41:31 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Home from "scenes/home";
 import Login from "scenes/login";
 import Profile from "scenes/profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
   return (
