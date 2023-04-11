e2989ede45cd383a73fb4f520cf03122fbf3819a
Megan <meganthielman@gmail.com>
2023-04-10 18:43:44 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { themeSettings } from "theme";
 
 function App() {
   const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className="app">
